99539dc9b7fafe4b433b63ae1b6ec6dd20338e71
juliaR29 <juliamreis@gmail.com>
2022-12-12 22:34:55 -0300

    Informacoes do pet

--- lib/models/dataModel.dart
+++ lib/models/dataModel.dart
@@ -4,14 +4,12 @@ class DataModel {
   String name;
   String logo;
   String description;
-  String distance;
   List packages;
 
   DataModel({
     this.name,
     this.description,
     this.logo,
-    this.distance,
     this.packages
   });
 
@@ -21,7 +19,6 @@ class DataModel {
       name: parsedJson['name'],
       logo : parsedJson['logo'],
       description : parsedJson['description'],
-      distance : parsedJson['distance'],
       packages : parsedJson['packages'],
     );
   }
